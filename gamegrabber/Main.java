050b540f91b5446489db31fe72ee802d268b3641
Maurice-Yu <114034748+Maurice-Yu@users.noreply.github.com>
2022-09-20 21:32:23 -0500

    Update Main.java

--- gamegrabber/Main.java
+++ gamegrabber/Main.java
@@ -351,38 +351,38 @@ class Main
         // TEST 1: this expects exact matches.
 
 
-        String input = "-10\n2\nvalid\nwin\n1\nquit\n7\n3\n";
-        // 10
-        // 2
-        // valid
-        // win
-        // 1
-        // quit
-        // 7
-        // 3
+//         String input = "-10\n2\nvalid\nwin\n1\nquit\n7\n3\n";
+//         // 10
+//         // 2
+//         // valid
+//         // win
+//         // 1
+//         // quit
+//         // 7
+//         // 3
 
-        System.out.println("Test 1");
-        // setting this one aside so I can check if it's been played
-        DummyGame special = new DummyGame("Special");
+//         System.out.println("Test 1");
+//         // setting this one aside so I can check if it's been played
+//         DummyGame special = new DummyGame("Special");
 
-        System.out.println("special.isOver: "+special.isOver());
+//         System.out.println("special.isOver: "+special.isOver());
 
 
-        Game[] games = new Game[]{new DummyGame("apples"),
-                           new DummyGame("Bananas"),
-                           special};
-        Scanner scan = new Scanner(input);
-        GameGrabber gg = new GameGrabber(games, scan);
-        gg.doMenu();
+//         Game[] games = new Game[]{new DummyGame("apples"),
+//                            new DummyGame("Bananas"),
+//                            special};
+//         Scanner scan = new Scanner(input);
+//         GameGrabber gg = new GameGrabber(games, scan);
+//         gg.doMenu();
 
-        System.out.println("special.isOver: "+special.isOver());
+//         System.out.println("special.isOver: "+special.isOver());
 
-        System.out.println("Test 2");
-        System.out.println("This requires manual validation and input");
-        System.out.println("Check for a well-formatted menu with at least 4 options, one for each game.");
-        // in case you didn't know -- you can call main directly. While it is a _special_ static method,
-        // it is, ultimately, just a static method
-        GameGrabber.main(new String[0]);
+//         System.out.println("Test 2");
+//         System.out.println("This requires manual validation and input");
+//         System.out.println("Check for a well-formatted menu with at least 4 options, one for each game.");
+//         // in case you didn't know -- you can call main directly. While it is a _special_ static method,
+//         // it is, ultimately, just a static method
+//         GameGrabber.main(new String[0]);
     }
 
   //   public static void main(String[] args)
